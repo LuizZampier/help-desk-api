bb8778aaa1c9e9b79fc4d3f7f946cd46838cb20f
Luiz Zampier <luizzampier17@gmail.com>
2025-11-07 10:28:37 -0300

    build: jest and cors added

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from "express"
 import "express-async-errors"
+import cors from "cors"
 
 import { routes } from "./routes"
 import { errorHandling } from "./middlewares/errorHandling"
@@ -7,7 +8,9 @@ import uploadConfig from "./configs/upload"
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
+
 app.use("/uploads", express.static(uploadConfig.UPLOADS_FOLDER))
 
 app.use(routes)
